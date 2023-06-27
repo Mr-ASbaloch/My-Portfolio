dc3bdf27bfc3486efbc0ddd8117fc2195cc2f271
abdul <abdulsaeed1356@gmail.com>
2023-06-27 14:05:48 +0500

    work done

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
 //     prevEl: ".swiper-button-prev",
 //   },
 // });
+
+
+
+//  scroll up button 
+
+var scrollTop = document.getElementById("scrollTop");
+
+window.onscroll = function(){
+    scrollfunction()
+};
+function scrollfunction(){
+
+    if( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        scrollTop.style.display = "block";
+    } else {
+        scrollTop.style.display = "none";
+    }
+}
+
+scrollTop.addEventListener("click", function(){
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    })
+})
