6d58bfce967612214be4c743f476a6fb51a31388
abdul <abdulsaeed1356@gmail.com>
2023-07-01 09:00:56 +0500

    portfolio work

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 10,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
-
-
-//  scroll up button 
-
-var scrollTop = document.getElementById("scrollTop");
-
-window.onscroll = function(){
-    scrollfunction()
-};
-function scrollfunction(){
-
-    if( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        scrollTop.style.display = "block";
-    } else {
-        scrollTop.style.display = "none";
-    }
+toggle = document.querySelector(".toggler");
+toggle.onclick = function() { 
+    navBar= document.querySelector (".nav");
+    navBar.classList.toggle("active")
 }
-
-scrollTop.addEventListener("click", function(){
-    window.scrollTo({
-        left: 0,
-        top: 0,
-        behavior: "smooth"
-    })
-})
