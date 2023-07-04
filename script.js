a0a89dfad3aab29c17eda05fe9a68e81f4849fd2
abdul <abdulsaeed1356@gmail.com>
2023-07-04 12:06:15 +0500

    portfolio work

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ toggle.onclick = function() {
     navBar= document.querySelector (".nav");
     navBar.classList.toggle("active")
 }
+
+// pop up 
+
+const popup = document.getElementById("popup");
+const readMoreBtn = document.getElementById("read-more-btn");
+
+readMoreBtn.addEventListener("click", function() {
+  popup.style.display = "block";
+});
+
+function closePopup() {
+  popup.style.display = "none";
+}
